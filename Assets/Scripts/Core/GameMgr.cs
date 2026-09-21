e3c1748f38ee0e70825b36a61fa4f885c2927ada
daile.zeng <3162816300@qq.com>
2026-09-21 15:29:50 +0800

    fix：修复提前cull

--- Assets/Scripts/Core/GameMgr.cs
+++ Assets/Scripts/Core/GameMgr.cs
@@ -65,7 +65,7 @@ namespace Core
             base.Awake();
             if (Instance != this) return;
 
-            QualitySettings.vSyncCount = 0; Application.targetFrameRate = 60; QualitySettings.SetQualityLevel(0);
+            QualitySettings.vSyncCount = 0; Application.targetFrameRate = 60;
             _cfg = GameConfig.Get();
             _model = new GameModel();
             _input = new InputController();
